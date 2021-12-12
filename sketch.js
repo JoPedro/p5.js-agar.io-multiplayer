5ba5c6ff5549c6ada4b5624cd73d70968903ecb0
João Pedro <joaopedrosilvaqueiroz@gmail.com>
2021-12-12 15:08:19 -0300

    Basic game working

--- sketch.js
+++ sketch.js
@@ -1,18 +1,32 @@
 var blob;
 var blobs = [];
+var zoom = 1;
 
 function setup() {
     createCanvas(600, 600);
-    blob = new Blob(width/2, width/2, 64);
-    for (var i = 0; i < 10; i++) {
-        blobs[i] = new Blob(random(width), random(height), 16);
+    blob = new Blob(0, 0, 32);
+    for (var i = 0; i < 200; i++) {
+        var x = random(-width*2, width*2);
+        var y = random(-height*2, height*2);
+        blobs[i] = new Blob(x, y, 16);
     }
 }
 
 function draw() {
     background(0);
+    
+    translate(width/2, height/2);
+    var newzoom = 32 / blob.r;
+    zoom = lerp(zoom, newzoom, 0.1);
+    scale(zoom);
+    translate(-blob.pos.x, -blob.pos.y);
+    
     blob.show();
-    for (var i = 0; i < blobs.length; i++) {
+    blob.update();
+    for (var i = blobs.length-1; i >= 0; i--) {
         blobs[i].show();
+        if (blob.eats(blobs[i])) {
+            blobs.splice(i, 1);
+        }
     }
 }
